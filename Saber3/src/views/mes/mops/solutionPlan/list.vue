53d1dd1aff510a1f9d389ff23dba6bfdc54c77c8
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2025-08-01 09:55:35 +0800

    refactor: share material label dialog

--- Saber3/src/views/mes/mops/solutionPlan/list.vue
+++ Saber3/src/views/mes/mops/solutionPlan/list.vue
@@ -110,7 +110,7 @@
 <script>
 import listPage from '@/mixins/list-page';
 import options from './list';
-import materialLabelDialog from './cpns/materialLabelDialog.vue';
+import materialLabelDialog from '../cpns/materialLabelDialog.vue';
 export default {
   mixins: [listPage],
   components: { materialLabelDialog },
