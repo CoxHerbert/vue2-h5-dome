f48434f4d545a71a42cd74d51a342c86edebd3ab
鲍泽楠 <coxherbert98@gmail.com>
2025-10-17 09:39:37 +0800

    1

--- src/views/apps/index.vue
+++ src/views/apps/index.vue
@@ -2,13 +2,7 @@
   <div class="page apps">
     <ScrollAwareNavBar title="应用" fixed />
     <div class="apps__content">
-      <van-grid
-        class="apps__grid"
-        :column-num="4"
-        :gutter="12"
-        clickable
-        :border="false"
-      >
+      <van-grid class="apps__grid" :column-num="4" :gutter="12" clickable :border="false">
         <van-grid-item v-for="app in apps" :key="app.routeName" :to="{ name: app.routeName }">
           <template #icon>
             <img :src="app.icon" :alt="app.label" class="apps__icon" loading="lazy" />
@@ -30,7 +24,11 @@ const apps = [
   { label: '现场计划单', icon: '/images/apps/现场计划单.svg', routeName: 'appsSitePlanning' },
   { label: '工时汇报', icon: '/images/apps/工时汇报.svg', routeName: 'appsWorkReport' },
   { label: '确认领料', icon: '/images/apps/确认领料.svg', routeName: 'appsPickingConfirmation' },
-  { label: '物料信息维护', icon: '/images/apps/物料信息维护.svg', routeName: 'appsMaterialMaintenance' },
+  {
+    label: '物料信息维护',
+    icon: '/images/apps/物料信息维护.svg',
+    routeName: 'appsMaterialMaintenance',
+  },
   { label: '线材质检', icon: '/images/apps/线材质检.svg', routeName: 'appsWireInspection' },
   { label: '出货资料上传', icon: '/images/apps/出货资料上传.svg', routeName: 'appsShipmentUpload' },
   { label: '外协核价', icon: '/images/apps/外协核价.svg', routeName: 'appsOutsourcingQuotation' },
@@ -50,7 +48,12 @@ const apps = [
   padding-top: calc(72px + var(--van-safe-area-top, 0px));
   box-sizing: border-box;
 }
-
+:deep(.van-grid-item) {
+  padding: 0;
+}
+:deep(.van-grid-item__content) {
+  padding: 0;
+}
 .apps__grid {
   background: #ffffff;
   border-radius: 16px;
