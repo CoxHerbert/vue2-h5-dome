657a3d77bc4ba9d3a12ae5dce8e5402d0bcd29d9
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2025-10-17 10:32:30 +0800

    refactor: auto register dc-ui components

--- src/components/dc-ui/index.js
+++ src/components/dc-ui/index.js
@@ -1,4 +1,32 @@
-export { default as Dict } from './components/Dict.vue';
-export { default as RecruitForm } from './components/RecruitForm.vue';
-export { default as Uploader } from './components/Uploader.vue';
-export { default as DcPagination } from './components/DcPagination.vue';
+const componentModules = import.meta.glob('./components/*.vue', { eager: true });
+
+function toKebabCase(name) {
+  return name
+    .replace(/^Dc/, '')
+    .replace(/([a-z\d])([A-Z])/g, '$1-$2')
+    .replace(/_/g, '-')
+    .toLowerCase();
+}
+
+export const dcComponents = Object.entries(componentModules).reduce(
+  (acc, [path, module]) => {
+    const component = module.default;
+    if (!component) {
+      return acc;
+    }
+
+    const fileName = path.split('/').pop().replace(/\.vue$/, '');
+    const componentName = component.name || fileName;
+    const tagName = `dc-${toKebabCase(componentName)}`;
+
+    if (!component.name) {
+      component.name = componentName;
+    }
+
+    acc[tagName] = component;
+    return acc;
+  },
+  {}
+);
+
+export default dcComponents;
--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,8 @@
-import { Dict, RecruitForm, Uploader, DcPagination } from './dc-ui';
+import dcUiComponents from './dc-ui';
 import LanguageSelector from './LanguageSelector.vue';
 import ScrollAwareNavBar from './ScrollAwareNavBar.vue';
 
 export function registerComponents(app) {
-  const dcUiComponents = {
-    'dc-dict': Dict,
-    'dc-recruit-form': RecruitForm,
-    'dc-uploader': Uploader,
-    'dc-pagination': DcPagination,
-  };
-
   Object.entries(dcUiComponents).forEach(([name, component]) => {
     app.component(name, component);
   });
