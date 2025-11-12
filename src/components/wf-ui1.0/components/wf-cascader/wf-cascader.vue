73fd77c80588a08693e40452e52871fcc8d81463
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2025-11-12 11:56:29 +0800

    refactor: migrate wf cascader to vant

--- src/components/wf-ui1.0/components/wf-cascader/wf-cascader.vue
+++ src/components/wf-ui1.0/components/wf-cascader/wf-cascader.vue
@@ -1,61 +1,66 @@
 <template>
-	<view class="wf-cascader">
-		<u-input
-			v-model="textLabel"
-			type="select"
-			:placeholder="getPlaceholder(column, column.type)"
-			@click="onClick"
-		/>
-		<u-popup v-model="show" mode="bottom" close-icon="close-circle" closeable>
-			<cascader :props="column.props" :itemList="dic" :title="column.label" @complete="onConfirm"></cascader>
-		</u-popup>
-	</view>
+  <div class="wf-cascader">
+    <u-input
+      v-model="textLabel"
+      type="select"
+      :placeholder="getPlaceholder(column, column.type)"
+      :disabled="disabled"
+      @click="onClick"
+    />
+    <u-popup v-model="show" mode="bottom" closeable>
+      <cascader
+        :props="column.props || {}"
+        :item-list="dic"
+        :title="column.label"
+        @complete="onConfirm"
+      />
+    </u-popup>
+  </div>
 </template>
 
 <script>
-import Props from '../../mixins/props.js'
+import Props from '../../mixins/props.js';
+import Cascader from './components/cascader.vue';
 
-import Cascader from './components/cascader'
 export default {
-	name: 'wf-cascader',
-	components: { Cascader },
-	mixins: [Props],
-	watch: {
-		dic: {
-			handler(val) {
-					if (!this.validateNull(val)) this.initTextLabel()
-			},
-			deep: true
-		}
-	},
-	data() {
-		return {
-			show: false,
-			textLabel: '',
-		}
-	},
-	methods: {
-		onClick() {
-			if (!this.disabled) this.show = true
-			this.handleClick()
-		},
-		onConfirm(val) {
-			const { result } = val
-			const value = val[this.valueKey]
-			if (this.column.type == 'cascader') this.text = result.map(r => r[this.valueKey])
-			else this.text = value
-			this.show = false
-		}
-	}
-}
+  name: 'wf-cascader',
+  components: { Cascader },
+  mixins: [Props],
+  data() {
+    return {
+      show: false,
+    };
+  },
+  watch: {
+    dic: {
+      handler(val) {
+        if (!this.validateNull(val)) this.initTextLabel();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    onClick() {
+      if (!this.disabled) this.show = true;
+      this.handleClick();
+    },
+    onConfirm(val) {
+      const { result = [] } = val || {};
+      const value = val ? val[this.valueKey] : undefined;
+      if (this.column.type === 'cascader') {
+        this.text = result.map((r) => r[this.valueKey]);
+      } else {
+        this.text = value;
+      }
+      this.textLabel = val ? val[this.labelKey] || '' : '';
+      this.show = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .wf-cascader {
-	width: 100%;
-
-	::v-deep.u-close--top-right {
-		top: 20rpx;
-	}
+  width: 100%;
 }
 </style>
