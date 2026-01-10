f2b99c8876b233bbd8a34688005753cfd740641e
鲍泽楠 <coxherbert98@gmail.com>
2026-01-10 09:06:10 +0800

    fix

--- src/api/modules/message.js
+++ src/api/modules/message.js
@@ -2,11 +2,11 @@ import request from '@/utils/http';
 
 export default {
   // 企微推送卡片消息
-  sendWechatMessage(params) {
+  sendWechatMessage(data) {
     return request({
       url: '/blade-bip/wechat/message/send',
-      method: 'GET',
-      params,
+      method: 'post',
+      data,
     });
   },
 };
