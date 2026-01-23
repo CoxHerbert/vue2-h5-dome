c6961aa35f0aa70e90d876e15ce2546f90b78148
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2026-01-23 16:34:42 +0800

    feat: prefix storage and cookie keys

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,32 +1,29 @@
 import Cookies from 'js-cookie';
 
-const TokenKey = 'saber3-access-token';
-const RefreshTokenKey = 'saber3-refresh-token';
-const SessionId = 'JSESSIONID';
-const UserId = 'b-user-id';
+import { KEYS } from '@/constants/keys';
 
 export function getToken() {
-  return Cookies.get(TokenKey);
+  return Cookies.get(KEYS.ACCESS_TOKEN);
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token);
+  return Cookies.set(KEYS.ACCESS_TOKEN, token);
 }
 
 export function getRefreshToken() {
-  return Cookies.get(RefreshTokenKey);
+  return Cookies.get(KEYS.REFRESH_TOKEN);
 }
 
 export function setRefreshToken(token) {
-  return Cookies.set(RefreshTokenKey, token);
+  return Cookies.set(KEYS.REFRESH_TOKEN, token);
 }
 
 export function removeToken() {
-  Cookies.remove(SessionId);
-  Cookies.remove(UserId);
-  return Cookies.remove(TokenKey);
+  Cookies.remove(KEYS.SESSION_ID);
+  Cookies.remove(KEYS.USER_ID);
+  return Cookies.remove(KEYS.ACCESS_TOKEN);
 }
 
 export function removeRefreshToken() {
-  return Cookies.remove(RefreshTokenKey);
+  return Cookies.remove(KEYS.REFRESH_TOKEN);
 }
